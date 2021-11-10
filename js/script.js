42f7bf14eefa015b330f66674f18c9c1442d49ff
G-Salis <giuseppesalislavoro@gmail.com>
2021-11-10 14:56:11 +0100

    Hello World!

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+   el: '#app',
+
+   data: {
+       msg: 'Hello world!',
+       img: 'https://www.raiplayradio.it/dl/img/2018/04/19/1524147442011_1280x720_1524064727557_earth%20day.jpg',
+   }
+
+})
